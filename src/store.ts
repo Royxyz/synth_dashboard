85a2d75f17e6a7a2c98364bd57901de66311fb9a
Royxyz <roy.ritabrata.04@gmail.com>
2026-09-13 15:20:12 +0530

    stable

--- src/store.ts
+++ src/store.ts
@@ -2,17 +2,8 @@ export const StateStore = {
     telemetry: {
         cutoff: 0,
         res: 0,
-        morph1: 0,
-        morph2: 0,
-        lfo1Rate: 0,
-        lfo2Rate: 0,
-        oscMix: 0,
-        masterFxMix: 0,
-        osc1Bank: 0,
-        osc2Bank: 0,
-        filterMode: 0,
-        lfo1Wave: 0,
-        lfo2Wave: 0,
+        menuPage: 0,
+        midiSource: 0,
         currentStep: 0
     }
 };
